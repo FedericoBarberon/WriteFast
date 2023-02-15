ff3d8819d6479374f313a522d4983501514c922d
Federico Barberon <jfedericobarberonj@gmail.com>
2023-02-15 21:38:37 +0000

    Separate the login in a custom hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,79 +1,28 @@
-import { useEffect } from "react";
-import { useState } from "react";
-import classes from "./app.module.css";
-import Input from "./components/Input/Input";
-import Loading from "./components/Loading/Loading";
-import Results from "./components/Results/Results";
-import SelectTime from "./components/SelectTime/SelectTime";
-import WordsVisor from "./components/WordsVisor/WordsVisor";
-import { getWords } from "./services/getWords";
+import { useState } from 'react'
+import { useWpm } from './hooks/useWpm'
+import classes from './app.module.css'
+import Input from './components/Input/Input'
+import Loading from './components/Loading/Loading'
+import Results from './components/Results/Results'
+import SelectTime from './components/SelectTime/SelectTime'
+import WordsVisor from './components/WordsVisor/WordsVisor'
 
-export default function App() {
-  const [started, setStarted] = useState(false);
-  const [showResults, setShowResults] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [buffer, setBuffer] = useState("");
-  const [words, setWords] = useState([]);
-  const [writeStatus, setWriteStatus] = useState();
-  const [errors, setErrors] = useState(0);
-  const [corrects, setCorrects] = useState(0);
-  const [initialTime, setInitialTime] = useState(60);
-  const [time, setTime] = useState(initialTime);
-
-  const startGame = async () => {
-    setLoading(true);
-    const fetchedWords = await getWords();
-    setLoading(false);
-
-    setTime(initialTime);
-    setWords(fetchedWords);
-    setStarted(true);
-  };
-
-  const resetGame = () => {
-    setStarted(false);
-    setShowResults(false);
-    setBuffer("");
-    setWriteStatus();
-    setErrors(0);
-    setCorrects(0);
-    setTime(initialTime);
-  };
-
-  const handleChange = (event) => {
-    if (event.target.value === " ") return;
-    setBuffer(event.target.value);
-  };
-
-  useEffect(() => {
-    if (!started) return;
-
-    if (time > 0) {
-      const interval = setInterval(() => {
-        setTime(time - 1);
-      }, 1000);
-
-      return () => clearInterval(interval);
-    } else {
-      setStarted(false);
-      setShowResults(true);
-    }
-  }, [started, time]);
-
-  useEffect(() => {
-    if (buffer.at(-1) === " ") {
-      if (words[0] === buffer.slice(0, -1)) {
-        setWriteStatus("success");
-        setCorrects(corrects + 1);
-      } else {
-        setWriteStatus("error");
-        setErrors(errors + 1);
-      }
-
-      setBuffer("");
-      setWords((words) => words.slice(1));
-    }
-  }, [buffer]);
+export default function App () {
+  const [initialTime, setInitialTime] = useState(60)
+  const {
+    started,
+    startGame,
+    resetGame,
+    time,
+    buffer,
+    updateBuffer,
+    words,
+    writeStatus,
+    corrects,
+    errors,
+    loading,
+    showResults
+  } = useWpm()
 
   return (
     <main className={classes.container}>
@@ -82,32 +31,34 @@ export default function App() {
 
       {loading && <Loading />}
 
-      {started ? (
-        <>
-          <WordsVisor words={words} />
-          <div className={classes.row}>
-            <Input
-              buffer={buffer}
-              handleChange={handleChange}
-              writeStatus={writeStatus}
+      {started
+        ? (
+          <>
+            <WordsVisor words={words} />
+            <div className={classes.row}>
+              <Input
+                buffer={buffer}
+                handleChange={updateBuffer}
+                writeStatus={writeStatus}
+              />
+              <span className={classes.time}>{time}s</span>
+              <button className='button' onClick={resetGame}>
+                Stop
+              </button>
+            </div>
+          </>
+          )
+        : (
+          <>
+            <SelectTime
+              initialTime={initialTime}
+              handleChange={(e) => setInitialTime(e.target.value)}
             />
-            <span className={classes.time}>{time}s</span>
-            <button className="button" onClick={resetGame}>
-              Stop
+            <button className='button' onClick={() => startGame(initialTime)}>
+              Empezar
             </button>
-          </div>
-        </>
-      ) : (
-        <>
-          <SelectTime
-            initialTime={initialTime}
-            handleChange={(e) => setInitialTime(e.target.value)}
-          />
-          <button className="button" onClick={startGame}>
-            Empezar
-          </button>
-        </>
-      )}
+          </>
+          )}
 
       {showResults && (
         <Results
@@ -118,5 +69,5 @@ export default function App() {
         />
       )}
     </main>
-  );
+  )
 }
